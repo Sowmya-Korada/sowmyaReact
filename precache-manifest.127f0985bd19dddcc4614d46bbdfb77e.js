927c8b3ae9324a45e6d64161da3ae2db139dba39
Sowmya-Korada <sowmyakorada2000@gmail.com>
2020-06-25 11:11:55 +0530

    Updates

---
+++ precache-manifest.127f0985bd19dddcc4614d46bbdfb77e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ed38f213720cd361b55b7f018a1675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a04026bb051f84b79a6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7ccaafdf747f5751f2b9",
+    "url": "/static/js/2.eb1d280c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb1d280c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a04026bb051f84b79a6",
+    "url": "/static/js/main.a6e0e67c.chunk.js"
+  },
+  {
+    "revision": "db5ce49a0a74e7bfa355",
+    "url": "/static/js/runtime-main.b4bdfd71.js"
+  },
+  {
+    "revision": "77d9312a554f7e61aa44ff063aeaf633",
+    "url": "/static/media/man.77d9312a.svg"
+  },
+  {
+    "revision": "9e379c2b5e7811c433ff8a4daeb1286a",
+    "url": "/static/media/women.9e379c2b.svg"
+  }
+]);
