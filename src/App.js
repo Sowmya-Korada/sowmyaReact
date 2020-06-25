722027c16b3d1963b7cc4dd3cc32efa22e3f5a41
Sowmya-Korada <sowmyakorada2000@gmail.com>
2020-06-25 11:10:56 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ import data from './data/data.json';
+ import profileIcon from './man.svg';
+ import profileIcon1 from './women.svg';
+ import {BrowserRouter,Route,Switch,Link} from 'react-router-dom';
+ import Resume from './Resume';
+import Home from './Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+//import SC from './StatesConcept'
+ let App=()=>{
 
-export default App;
+   return(
+
+        <BrowserRouter>
+        <Switch>
+           <Route exact path='/home' component={Home} />
+           <Route exact path='/resume' component={Resume} />
+        </Switch>
+        </BrowserRouter>
+
+   )
+ }
+
+ // let Example=()=>{
+ //   return(
+ //     <div>
+ //         <h2>Example Component</h2>
+ //     </div>
+ //   )
+ // }
+
+
+
+
+
+
+ export default App;
